8c0c7f8945ed15927af3458319ea81f4e738762e
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-08-22 20:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your JS code here. If required.
+
+
+const filterOddNumber = (arr)=> {
+  return arr.filter((num)=>num % 2 == 0)
+  
+};
+
+const multiplyBy2 = (arr)=>{
+    return arr.map((num)=> num * 2);
+}
+
+
+const manipulateArr = (arr)=>{
+  return new Promise((resolve,reject)=>{
+    
+    setTimeout(()=>{
+      const filterNum = filterOddNumber(arr);
+      document.getElementById("output").innerText = filterNum;
+      
+      setTimeout(()=>{
+        const multiple = multiplyBy2(arr);
+        
+        document.getElementById("output").innerText = multiple;
+        
+         setTimeout(() => {
+                            resolve(multiple);
+            }, 1000); 
+      },1000)
+    },1000)
+  })
+}
+let arr = [1,2,3,4]
+
+  manipulateArr(arr).then(finalArray => {
+    
+            document.getElementById('output').innerText =finalArray;
+        });
